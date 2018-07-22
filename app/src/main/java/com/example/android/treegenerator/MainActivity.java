9952aa04fac31fa372b234347217ed6e17d7d784
James Chen <zc1069@nyu.edu>
2018-07-22 01:00:17 -0400

    SeekBar no such instance field
    No such instance field.

--- app/src/main/java/com/example/android/treegenerator/MainActivity.java
+++ app/src/main/java/com/example/android/treegenerator/MainActivity.java
@@ -1,20 +1,63 @@
 package com.example.android.treegenerator;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Canvas mCanvas;
+    private Paint mPaint;
+    private Bitmap mBitmap;
+    private ImageView mImageView;
+
+    private double angle = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SeekBar mSeekBar;
+
         super.onCreate(savedInstanceState);
-        setContentView(new myClass(this));
+
+        mImageView = (ImageView) findViewById(R.id.imageView);
+        mSeekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        // set up for the seekbar
+        // TODO why can'r the program find the mSeekBar object
+        // TODO how to refresh the tree image according to the angle from the seekbar
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            // Get the value from the seekBar from user input and store it in a double angle.
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int seekBarValue, boolean fromUser) {
+                angle = seekBarValue;
+            }
+
+            // this function is not necessary.
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "start seek bar", Toast.LENGTH_SHORT).show();
+
+            }
+
+            // this function is not necessary
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "end seek bar", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        setContentView(R.layout.activity_main);
+        //setContentView(new myClass(this));
     }
 
 
